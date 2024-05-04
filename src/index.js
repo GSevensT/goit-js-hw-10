3b6dc151d11620e21c3e5e5cf796f3a961e946a1
GSevensT <143946090+GSevensT@users.noreply.github.com>
2024-05-04 20:34:37 +0800

    update

--- src/index.js
+++ src/index.js
@@ -11,12 +11,41 @@ function chooseBreed() {
   fetchBreeds().then(data => {
     loaderEl.classList.replace('loader', 'is-hidden');
 
-    let optionsMarkup = data.map(({ id, name }) => {
-      return `<options value=${id}>${name}</options>`;
-    });
+    let optionsMarkup = data
+      .map(({ id, name }) => {
+        return `<options value=${id}>${name}</options>`;
+      })
+      .join('');
     breedSelectEl.insertAdjacentHTML('beforeend', optionsMarkup);
     breedSelectEl.classList.remove('is-hidden');
   });
 }
 
 chooseBreed();
+
+breedSeletEl.addEventListener('change', e => {
+  //show loader
+  loaderEl.classList.replace('is-hidden', 'loader');
+
+  //hide select elem
+  catInfoEl.classList.add('is-hidden');
+
+  let breedId = e.target.value;
+
+  fetchCatByBreed(breedId).then(data => {
+    const { url, breeds } = data[0];
+    //console.log(breeds);
+    //return;
+    const { name, description, temperament } = breeds[0];
+    catInfoEl.innerHTML = `
+    <img src=${url} alt=${name} width="400"/>
+    <div>
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <p>${temperament}</p>
+    </div>
+    `;
+    catInfoEl.classList.remove('is-hidden');
+    loaderEl.classList.add('is-hidden');
+  });
+});
